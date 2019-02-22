f621988cd003f7cfb5b1d72df54ddc8598ac9ac8
xqyjjq <walk_code@163.com>
2019-02-22 10:30:59 +0800

    delete invalid project

--- dodo-rebate-rebot-api/src/main/java/com/dodo/project/rebate/rebot/api/Application.java
+++ dodo-rebate-rebot-api/src/main/java/com/dodo/project/rebate/rebot/api/Application.java
@@ -1,7 +1,5 @@
 package com.dodo.project.rebate.rebot.api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -14,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  * 项目启动类
  * </pre>
  *
- * @Author xqyjjq walk_code@163.com
+ * @Author xqyjjq walk_code@163.comdodo--rebate-rebot-api
  * @Date 2019/2/20 13:50
  * @Since JDK 1.8
  */
